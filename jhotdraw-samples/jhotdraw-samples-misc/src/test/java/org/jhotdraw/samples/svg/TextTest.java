e366a8b7d42141e2b562207e566778f55bb06379
tsoer18 <tsoer18@student.sdu.dk>
2023-02-16 22:25:20 +0100

    adding test

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/TextTest.java
@@ -0,0 +1,18 @@
+package org.jhotdraw.samples.svg;
+
+import org.jhotdraw.draw.figure.TextAreaFigure;
+import org.jhotdraw.draw.figure.TextFigure;
+import org.junit.Test;
+
+
+public class TextTest {
+    @Test
+    public void textTestMain(){
+
+        TextFigure textFigure = new TextFigure("hello");
+        if(textFigure.getText() == "hello"){
+            System.out.println("true and based");
+            assert(true);
+        }
+    }
+}
